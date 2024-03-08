9ebaa8c4c737cfec03d9f1ed7e805e5d0a4cb270
ViacheslavKosh <koshelevskiy.v@gmail.com>
2024-03-08 10:24:56 +0200

    08032024

--- task_01_03.py
+++ task_01_03.py
@@ -2,18 +2,33 @@
 
 from datetime import datetime
 
+# Виводить привітання
 print("Hello! Now we will count days from date you type!")
 
+# Користувач вводить дату у форматі РРРР-ММ-ДД
 date = str(input("Enter date using format 'YYYY-MM-DD': "))
 
 def get_days_from_today(date):
     try:
+
+        # Повертаємо рядок дати як об'єкт datetime    
         date_object = datetime.strptime(date, "%Y-%m-%d")
+        
+        # Отримуэмо поточну дату 
         date_today = datetime.today()
+        
+        # Використовуючи метод toordinal() отримуємо різницю в днях між датами як ціле число
         differecne = int(date_today.toordinal() - date_object.toordinal())
+        
+        # Виводимо на екран отриману різницю
         print(differecne)
+        
+        # Повертаємо отриману різницю
         return differecne
+    
+    # Якщо дата введена в невірному форматі то отримаемо зауваження
     except ValueError:
         print(f"Incorrect date format {date}. Try again with format: 'YYYY-MM-DD'")
 
+# Викликаємо функцію для дати введеної користувачем
 get_days_from_today(date)
