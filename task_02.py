5b000db149671164bd28acc5dfd2519fde278979
ViacheslavKosh <koshelevskiy.v@gmail.com>
2024-03-10 22:59:27 +0200

    finalization

--- task_02.py
+++ task_02.py
@@ -5,23 +5,27 @@ import random
 def get_numbers_tickets(min, max, quantity):
     
     # перевіряємо відповідність вхідних даних заданим параметрам (min>1, max<1000)
-    if min < 1 or max > 1000:
+    if min < 1 or max > 1000 :
         
         # повертаємо пустий список при невідповідності вхідних параметрів
         return []
     
     # создаємо список з послідовності чисел від min до max включно. Для включення останнього числа прописуємо 'max + 1'
     number_list = list(range(min, max + 1)) 
-    print(number_list)
     
-    # за допомогою метода sample пакета random відбираємо необхідну кількість чисел без повторень
-    ticket_numbers = random.sample(number_list, k=quantity)
-    print(ticket_numbers)
+    # додаємо обробку виключення для випадку вводу невірних даних
+    try:    
+        # за допомогою метода sample пакета random відбираємо необхідну кількість чисел без повторень
+        ticket_numbers = random.sample(number_list, k=quantity)
+        
+        # створюємо новий відсортований список чисел зі списку відібраних 
+        sorted_ticket_numbers = sorted(ticket_numbers)
     
-    # створюємо новий відсортований список чисел зі списку відібраних 
-    sorted_ticket_numbers = sorted(ticket_numbers)
+        # Повертаємо відсортований за порядком список відібраних чисел
+        return sorted_ticket_numbers
     
-    # Повертаємо відсортований за порядком список відібраних чисел
-    return sorted_ticket_numbers
+    # Виводимо попередження якщо кількість відбору вища за кількість чисел у списку
+    except ValueError:
+        print(f"Quantity of numbers must be less or equal then the largest number in the range")
 
-print(get_numbers_tickets(1, 1001, 10))
+print(get_numbers_tickets(1, 100, 1000))
